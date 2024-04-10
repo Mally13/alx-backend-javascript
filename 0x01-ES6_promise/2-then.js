8eaf0376376cc952f14decf8257f245adbe996a3
Mally13 <mallymutuku13@gmail.com>
2024-04-10 19:36:04 +0300

    Task 2: Catch me if you can

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(response => {
-    console.log('Got a response from the API');
-    return { status: 200, body: 'success' };
-  })
-  .catch(error => {
-    return (new Error(''));
-  });
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => new Error(''));
 }
