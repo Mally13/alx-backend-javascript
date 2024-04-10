463ca67278af879746a8244692519765c927e856
Mally13 <mallymutuku13@gmail.com>
2024-04-10 12:54:02 +0300

    Task 9: No need to create empty objects before adding in properties

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,15 @@
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const fullBudget = {
+    ...getBudgetObject(income, gdp, capita),
+    getIncomeInDollars(income) {
+        return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+        return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
