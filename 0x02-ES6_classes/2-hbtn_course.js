7f8e67ca3704d4f6e465123fb0861361e58917a0
Mally13 <mallymutuku13@gmail.com>
2024-04-12 02:06:46 +0300

    Task 2: getters and setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,52 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = this.validateName(name);
+    this._length = this.validateLength(length);
+    this._students = this.validateStudents(students);
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = this.validateName(value);
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    this._length = this.validateLength(value);
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    this._students = this.validateStudents(value);
+  }
+
+  validateName(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    return value;
+  }
+
+  validateLength(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    return value;
+  }
+
+  validateStudents(value) {
+    if (!Array.isArray(value) || !value.every(student => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    return value;
+  }
+}
