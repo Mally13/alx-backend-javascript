d70ef40ae967d6e62fa7333ff0b7e9a08ee73fb3
Mally13 <mallymutuku13@gmail.com>
2024-04-23 16:04:35 +0300

    Task 4 :promises

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,3 +1,9 @@
 export default function signUpUser(firstName, lastName) {
-  
+  return new Promise((resolve) => {
+    const user = {
+      firstName,
+      lastName,
+    };
+    resolve(user);
+  });
 }
