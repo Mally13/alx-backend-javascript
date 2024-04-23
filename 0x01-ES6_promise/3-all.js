db8f8a537bc06c872464b6d49b0d79979bda2295
Mally13 <mallymutuku13@gmail.com>
2024-04-23 15:32:35 +0300

    Task 3 :Handling multiple promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((response) => {
-      console.log(`${response[0].body} ${response[1].firstName} ${response[1].lastName}`);
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((photo, user) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
-    .catch(() => console.log('Signup system offline'));
+    .catch(() => {
+      console.log('Signup system offline')
+    });
 }
