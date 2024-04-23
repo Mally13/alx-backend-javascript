33d5262e946686d3c714d1679c19bdcaabdbaa85
Mally13 <mallymutuku13@gmail.com>
2024-04-23 19:12:03 +0300

    Task 6: Promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  const [userResult, photoResult] = await Promise.allSettled([userPromise, photoPromise]);
+
+  return [
+    { status: userResult.status, value: userResult.status === 'fulfilled' ? userResult.value : userResult.reason },
+    { status: photoResult.status, value: photoResult.status === 'fulfilled' ? photoResult.value : photoResult.reason },
+  ];
+}
