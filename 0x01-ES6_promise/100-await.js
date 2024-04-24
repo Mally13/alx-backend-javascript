faaaedfe39604cd519907ba37672a9cb3ecdee37
Mally13 <mallymutuku13@gmail.com>
2024-04-24 17:45:41 +0300

    Task 100: await

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,16 +1,19 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const [photoRes, userRes] = await Promise.all([uploadPhoto(), createUser()]);
-  console.log(photoRes);
-  return {
-    photo: {
-      status: photoRes.status,
-      body: photoRes.body,
-    },
-    user: {
-      firstName: userRes.firstName,
-      lastName: userRes.lastName,
-    },
-  };
+  try {
+    const [photoRes, userRes] = await Promise.all([uploadPhoto(), createUser()]);
+    return {
+      photo: {
+        status: photoRes.status,
+        body: photoRes.body,
+      },
+      user: {
+        firstName: userRes.firstName,
+        lastName: userRes.lastName,
+      },
+    };
+  } catch (error) {
+    return error.message;
+  }
 }
