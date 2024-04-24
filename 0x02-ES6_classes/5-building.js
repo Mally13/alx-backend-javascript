fd86b984c5183005857fffd2ddbef3d008d2bbfb
Mally13 <mallymutuku13@gmail.com>
2024-04-24 19:24:53 +0300

    Abstract classes

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+export default class Pricing {
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+
+  evacuationWarningMessage () {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
