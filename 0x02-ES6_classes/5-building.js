f83dfa71f31b40192647f07fbe323149f3746999
Mally13 <mallymutuku13@gmail.com>
2024-05-01 09:10:02 +0300

    Task 11

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,8 @@
 export default class Building {
   constructor(sqft) {
+    if (typeof sqft !== 'number') throw new Error();
     this._sqft = sqft;
+    this.evacuationWarningMessage();
   }
 
   get sqft() {
--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -2,6 +2,7 @@ import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
+    if (typeof floors !== 'number') throw new Error();
     super(sqft);
     this._floors = floors;
   }
