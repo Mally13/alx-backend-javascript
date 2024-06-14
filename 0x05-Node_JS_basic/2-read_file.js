8b48ad4b0429c7869651612c7551f1ec6b245ecf
Mally13 <mallymutuku13@gmail.com>
2024-06-14 19:18:00 +0300

    Task 1: Read synchronously

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const fileData = fs.readFileSync(path, 'utf8');
+    const lines = fileData.split('\n');
+    const studentLines = lines.filter((line) => line.trim() !== '').slice(1);    // let studentCount = 0;
+    const numberOfStudents = studentLines.length;
+    console.log(`Number of students: ${numberOfStudents}`);
+    const fields = {};
+    studentLines.forEach((line) => {
+      const [firstname, lastname, age, field] = line.split(',');
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstname);
+    });
+    for (const field in fields) {
+      if (Object.prototype.hasOwnProperty.call(fields, field)) {
+        const students = fields[field];
+        console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+      }
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
